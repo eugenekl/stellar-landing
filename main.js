8814da554501c67ccb5c6684feaccc5ecdd7a5c5
Eugene Lee <eugenelee905@gmail.com>
2019-12-23 14:06:14 -0500

    overflow edit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById("modal");
 const ctaButton = document.getElementById("cta_button");
 const modalCloseButton = document.getElementById("modal__closeBtn");
+const bodyContainer = document.getElementById("container");
 
 ctaButton.addEventListener("click", openModal);
 modalCloseButton.addEventListener("click", closeModal);
@@ -8,14 +9,17 @@ window.addEventListener("click", clickOutsideModal);
 
 function openModal() {
   modal.style.display = "block";
+  bodyContainer.style.overflow = "hidden";
 }
 
 function closeModal() {
   modal.style.display = "none";
+  bodyContainer.style.overflow = "visible";
 }
 
 function clickOutsideModal(event) {
   if (event.target === modal) {
     modal.style.display = "none";
+    bodyContainer.style.overflow = "visible";
   }
 }
